8bc9da4444309109ed63577e474503e1fe5b13aa
ruanyl <ruanyu1@gmail.com>
2015-03-19 23:59:39 +0200

    fetch rebalancing data

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
-var request = require('request');
-var casper = require('casper').create({
-  pageSettings: {
-    userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
+var cookie = require('./lib/cookie');
+var fetch = require('./lib/fetch');
+var data = require('./data/stock.json');
+
+cookie.getCookieStr('http://xueqiu.com', function(cookie) {
+  for(var key in data.combine) {
+    fetch.rebalancing({symbol: key, cookie: cookie}, function(combine) {
+      console.log(combine);
+      console.log('=================================================');
+    });
   }
-});
-
-casper.start('http://xueqiu.com/', function() {
-  this.echo(this.getHTML());
-  this.fill('form[action="/user/login"]', {username: 'ruanyu1@gmail.com', password: '44242863', remember_me: '1'}, true);
-});
-
-casper.then(function() {
-  var url = 'http://xueqiu.com/cubes/rebalancing/history.json?cube_symbol=ZH010389&count=20&page=1';
-  this.download(url, 'test.json');
-});
-
-casper.run(function() {
-  this.echo('Done').exit();
 });
