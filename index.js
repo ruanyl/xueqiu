d779436924569bfc1f129eeb5066f45da68a701a
ruanyl <ruanyu1@gmail.com>
2015-03-23 16:58:48 +0200

    use async to contrall request concurrency

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 var cookie = require('./lib/cookie');
 var fetch = require('./lib/fetch');
 var data = require('./data/stock.json');
+var async = require('async');
+
+var q = async.queue(function(task, next) {
+  fetch.rebalancing(task, function(data) {
+    console.log(data);
+    next();
+  })
+}, 10);
 
 cookie.getCookieStr('http://xueqiu.com', function(cookie) {
   for(var key in data.combine) {
-    fetch.rebalancing({symbol: key, cookie: cookie}, function(combine) {
-      console.log(combine);
-      console.log('=================================================');
-    });
+    q.push({symbol: key, cookie: cookie});
   }
 });
+
+q.drain = function() {
+  console.log('done');
+};
